f8cfb2c9981ffc4c7be9ef818240691e1cf41d33
Daniel Oliveira <danieliver2011@gmail.com>
2020-07-02 03:26:48 -0300

    change buildspec file

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var Message = mongoose.model('Message', {
   message: String
 })
 
-var dbUrl = 'mongodb://daniel:abc123@ds261817.mlab.com:61817/simple-chat'
+var dbUrl = `mongodb://daniel:abc123@ds261817.mlab.com:61817/simple-chat`
 
 app.get('/messages', (req, res) => {
   Message.find({}, (err, messages) => {
@@ -68,11 +68,12 @@ io.on('connection', () => {
 })
 
 mongoose.connect(dbUrl, {
-  useMongoClient: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }, (err) => {
   console.log('mongodb connected', err);
 })
 
-var server = http.listen(80, () => {
+var server = http.listen(3000, () => {
   console.log('server is running on port', server.address().port);
 });
